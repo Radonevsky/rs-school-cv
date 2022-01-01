41bb4f24e35ed6fc2f4dc217a5404376638a815e
Radonevsky <Radonevsky@users.noreply.github.com>
2022-01-02 02:46:24 +0300

    feat: add animaton for author, nav. Edit color text

--- src/js/wordsAnimation.js
+++ src/js/wordsAnimation.js
@@ -6,6 +6,7 @@ const logoMain = document.querySelector('.logo__main .animation')
 const logoCaption = document.querySelector('.logo__caption .animation')
 const authorTitleFirst = document.querySelector('.author__title .animation.first')
 const authorTitleLast = document.querySelector('.author__title .animation.last')
+const authorTitle = document.querySelector('.author__title')
 
 const logoMainSpaned = spanize_letters(logoMain)
 const logoCaptionSpaned = spanize_letters(logoCaption)
@@ -37,27 +38,78 @@ Array.from(logoCaptionSpaned.querySelectorAll('.letter')).forEach((el, idx) => {
     })
 })
 
-let images = gsap.utils.toArray(logoMainSpaned.querySelectorAll('.letter'));
-images.forEach(image => {
-    image.addEventListener("mouseenter", () => {
+//Header hover
+let logoMainText = gsap.utils.toArray(logoMainSpaned.querySelectorAll('.letter'));
+logoMainText.forEach(logoMainText => {
+    logoMainText.addEventListener("mouseenter", () => {
         // animate the "hovered" image
-        gsap.to(image, {top: 3, duration: .5, ease: "elastic"})
+        gsap.to(logoMainText, {top: 3, duration: .5, ease: "elastic"})
     })
-    image.addEventListener("mouseleave", () => {
+    logoMainText.addEventListener("mouseleave", () => {
         // animate the "hovered" image
-        gsap.to(image, {top: 0, duration: .5,  ease: "elastic"})
+        gsap.to(logoMainText, {top: 0, duration: .5,  ease: "elastic"})
+    })
+})
+
+//Nav hover
+let navItem = gsap.utils.toArray(document.querySelectorAll('.nav__list-item'));
+navItem.forEach(navItem => {
+    navItem.addEventListener("mouseenter", () => {
+        // animate the "hovered" image
+        gsap.to(navItem, {scale: 1.3, duration: .5, ease: "elastic"})
+    })
+    navItem.addEventListener("mouseleave", () => {
+        // animate the "hovered" image
+        gsap.to(navItem, {scale: 1, duration: .5,  ease: "elastic"})
     })
 })
 
 /*<=== Author ===>*/
-
 Array.from(authorTitleFirstSpaned.querySelectorAll('.letter')).forEach((el, idx) => {
     gsap.from(el, 1, {
         opacity: 0,
         scale: .7,
         bottom: -100,
-        ease: "elastic",
         rotation: 360,
         delay: idx * 0.1
     })
 })
+
+Array.from(authorTitleLastSpaned.querySelectorAll('.letter')).forEach((el, idx) => {
+    gsap.from(el, 1, {
+        opacity: 0,
+        scale: .7,
+        bottom: -100,
+        rotation: 360,
+        delay: idx * 0.1
+    })
+})
+
+//Author hover
+let authorText = gsap.utils.toArray(authorTitle.querySelectorAll('.letter'));
+authorText.forEach(authorText => {
+    authorText.addEventListener("mouseenter", () => {
+        // animate the "hovered" image
+        gsap.to(authorText, {duration: .2, scale: 1.3})
+    })
+    authorText.addEventListener("mouseleave", () => {
+        // animate the "hovered" image
+        gsap.to(authorText, {duration: .2, scale: 1,})
+    })
+})
+
+/*<=== Contacts ===>*/
+//Contacts-item hover
+let contactItems = gsap.utils.toArray(document.querySelectorAll('.contacts__item'));
+contactItems.forEach(contactItem => {
+    contactItem.addEventListener("mouseenter", () => {
+        // animate the "hovered" image
+        gsap.to(contactItem, {duration: .3, scale: 1.1, ease: 'elastic'})
+    })
+    contactItem.addEventListener("mouseleave", () => {
+        // animate the "hovered" image
+        gsap.to(contactItem, {duration: .3, scale: 1, ease: 'elastic'})
+    })
+})
+
+
