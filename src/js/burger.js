6dff67dc6f7013b9012c454999aac16097f9c976
Radonevsky <Radonevsky@users.noreply.github.com>
2022-01-03 02:07:42 +0300

    feat: fix about section

--- src/js/burger.js
+++ src/js/burger.js
@@ -19,3 +19,23 @@ nav.onclick = () => {
         activeToggle(nav, "active-burger")
     }
 }
+/*<=== Header ===>*/
+const header = document.querySelector('.header')
+const headerLogo = document.querySelector('.logo')
+const headerLogoCaption = document.querySelector('.logo__caption')
+const overlay = document.querySelector('.overlay')
+
+window.addEventListener('scroll', function() {
+    if (pageYOffset > headerLogo.offsetHeight + 30){
+        headerLogo.style.transform = 'rotate(0deg)'
+        headerLogo.style.backgroundColor = 'rgba(250, 171, 213, .5)'
+        headerLogo.style.position = 'fixed'
+        headerLogo.style.top = '0'
+        headerLogo.style.left = '0'
+    }
+    if (pageYOffset <= headerLogo.offsetHeight + 30){
+        headerLogo.style.transform = 'rotate(-10deg)'
+        headerLogo.style.backgroundColor = 'rgba(250, 171, 213, 0)'
+        headerLogo.style.position = 'relative'
+    }
+});
