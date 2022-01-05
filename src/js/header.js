713999eb90b9e317bb5c7ac6c74e2ddc2e60e98c
Radonevsky <Radonevsky@users.noreply.github.com>
2022-01-05 22:18:26 +0300

    fix: refactoring

--- src/js/header.js
+++ src/js/header.js
@@ -6,14 +6,12 @@ const overlay = document.querySelector('.overlay')
 window.addEventListener('scroll', function() {
     if (pageYOffset > headerLogo.offsetHeight + 30){
         headerLogo.style.transform = 'rotate(0deg)'
-        headerLogo.style.backgroundColor = 'rgba(250, 171, 213, .5)'
-        headerLogo.style.position = 'fixed'
-        headerLogo.style.top = '0'
-        headerLogo.style.left = '0'
+        header.style.background = '#253242'
+
     }
     if (pageYOffset <= headerLogo.offsetHeight + 30){
         headerLogo.style.transform = 'rotate(-10deg)'
-        headerLogo.style.backgroundColor = 'rgba(250, 171, 213, 0)'
-        headerLogo.style.position = 'relative'
+        header.style.background = 'none'
+
     }
 });
