0c298b5ba10c6549f9c892538f491b48418eade9
Radonevsky <Radonevsky@users.noreply.github.com>
2022-01-06 22:05:40 +0300

    fix: head height and animation for phone

--- src/js/header.js
+++ src/js/header.js
@@ -2,16 +2,35 @@ const header = document.querySelector('.header')
 const headerLogo = document.querySelector('.logo')
 const headerLogoCaption = document.querySelector('.logo__caption')
 const overlay = document.querySelector('.overlay')
+const phoneWidth = 599;
 
-window.addEventListener('scroll', function() {
-    if (pageYOffset > headerLogo.offsetHeight + 30){
-        headerLogo.style.transform = 'rotate(0deg)'
-        header.style.background = '#253242'
+console.log(document.documentElement.clientWidth)
+// For phone landscape up
+if (document.documentElement.clientWidth > phoneWidth) {
+    window.addEventListener('scroll', function () {
+        if (pageYOffset > headerLogo.offsetHeight + 30) {
+            headerLogo.style.transform = 'rotate(0deg)'
+            header.style.background = '#253242'
+            header.style.height = '100px'
+            header.style.paddingTop = '0'
+        }
+        if (pageYOffset <= headerLogo.offsetHeight + 30) {
+            headerLogo.style.transform = 'rotate(-10deg)'
+            header.style.background = 'none'
+            header.style.height = '140px'
+            header.style.paddingTop = '15px'
+        }
+    });
+}
 
-    }
-    if (pageYOffset <= headerLogo.offsetHeight + 30){
-        headerLogo.style.transform = 'rotate(-10deg)'
-        header.style.background = 'none'
-
-    }
-});
+// For phone only
+if (document.documentElement.clientWidth <= phoneWidth) {
+    window.addEventListener('scroll', function () {
+        if (pageYOffset > headerLogo.offsetHeight + 30) {
+            header.style.background = '#253242'
+        }
+        if (pageYOffset <= headerLogo.offsetHeight + 30) {
+            header.style.background = 'none'
+        }
+    });
+}
